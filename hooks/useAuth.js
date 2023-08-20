f7e200104718f7ecf909daeed1bca8ef4f52a567
hultmanoskar <oskar.hultman@edu.stockholm.se>
2023-08-21 00:27:51 +0200

    Latest changes.

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,13 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { createContext, useContext } from 'react'
+import * as AuthSession from 'expo-auth-session';
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+
+const signInWithGoogle = async() => {
+  
+}
+
   return (
     <AuthContext.Provider 
     value={{ 
-user: "Ogge"
+user: null
 
     }}>
       {children}

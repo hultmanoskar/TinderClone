5b3fe219eac23c672e4b02f9fd997d18c3e6326e
hultmanoskar <oskar.hultman@edu.stockholm.se>
2023-08-22 18:41:30 +0200

    Latest changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,52 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { AntDesign, Entypo, Ionicons} from '@expo/vector-icons'
 
 
 const HomeScreen = () => {
 const navigation = useNavigation();
 
+
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-  <Button title="Go to chat" onPress={() => navigation.navigate("Chat")} />
-    </View>
+    <SafeAreaView style={{}}>
+        {/* Header */}
+
+<View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 24, padding: 4}}>
+<TouchableOpacity>
+  <Image style={styles.smallImg} source={require('../assets/rihanna.jpg')}/>
+</TouchableOpacity>
+<TouchableOpacity>
+  <Image style={styles.logoImg} source={require('../assets/tinder-logo.png')}/>
+</TouchableOpacity>
+
+<TouchableOpacity>
+  <Ionicons name='chatbubbles-sharp' size={30} color="#FF5864" style={styles.chatIcon}/>
+</TouchableOpacity>
+</View>
+
+
+        {/* End of Header */}
+
+     {/*  <Text>HomeScreen</Text>
+  <Button title="Go to chat" onPress={() => navigation.navigate("Chat")} /> */}
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  smallImg: {
+    height: 40,
+    width: 40,
+    borderRadius: 40/2,
+  },
+  logoImg:{
+    height: 40,
+    width: 40,
+    borderRadius: 40/2,
+  },
+  
+})
